f052a80809eb0fc57ad6deccf6ec8a1bc2d2dcc0
Anoopsmohan <anoopmhn2009@gmail.com>
2016-04-19 20:01:15 +0530

    Snippets : tutorial-2

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,6 +1,7 @@
 __author__ = 'anoop.sm'
 
 from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
 
 from snippets import views_1
 
@@ -10,3 +11,4 @@ urlpatterns = [
     url(r'(?P<pk>[0-9]+)/$', views_1.snippet_detail)
 ]
 
+urlpatterns = format_suffix_patterns(urlpatterns)
